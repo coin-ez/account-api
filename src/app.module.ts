c0fd44c2221a6bd79c1d13b238b6a174d37c8403
taeilyoon <zkrxpdlf0808@gmail.com>
2024-06-02 23:25:11 +0900

    add admin js.
    TODO ::: FIX QueryFailedError: column User.tronAddress does not exist

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,9 +13,53 @@ import { InternalModule } from './internal/internal.module';
 import { UserMiddleware } from './user/user.middleware';
 import { UserModule } from './user/user.module';
 import { TronModule } from './tron/tron.module';
+import { User } from './user/user.entity';
+import { Magiclink } from './auth/magiclink/magiclink.entity';
+import { Session } from './auth/session/session.entity';
+
 
 @Module({
   imports: [
+    import('@adminjs/nestjs').then(({ AdminModule }) => {
+      return import('adminjs').then(({ AdminJS }) => {
+        return import('@adminjs/typeorm').then((AdminJSTypeORM) => {
+          AdminJS.registerAdapter({ Database: AdminJSTypeORM.Database, Resource: AdminJSTypeORM.Resource });
+          return AdminModule.createAdminAsync({
+            useFactory: () => ({
+              adminJsOptions: {
+                rootPath: '/admin',
+                resources: [
+                  {
+                    resource: User,
+                    options: {
+                      properties: {
+                        encryptedPassword: {
+                          isVisible: false,
+                        },
+                        password: {
+                          type: 'string',
+                          isVisible: {
+                            list: false,
+                            edit: true,
+                            filter: false,
+                            show: false,
+                          },
+                        },
+                      },
+                    },
+                  },{
+                    resource: Magiclink
+                  },
+                  {
+                    resource : Session
+                  }
+                ],
+              },
+            }),
+          });
+        });
+      });
+    }),
     NTAppModule,
     DatabaseModule,
     UserModule,
@@ -36,6 +80,16 @@ export class AppModule implements NestModule {
           path: '/',
           method: RequestMethod.GET,
         },
+        {
+          path: 'admin',
+          method: RequestMethod.ALL,
+        },
+
+        {
+          path: 'admin/*',
+          method: RequestMethod.ALL,
+        },
+       
         {
           path: ':version/internal',
           method: RequestMethod.POST,
